bec24314ce92b3fbbc072544706d835c10cdc291
just-bqn <self.bqnguyen@gmail.com>
2024-06-26 23:38:30 +0700

    Some commit.

--- modules/home-manager/packages/jeezyvim.nix
+++ modules/home-manager/packages/jeezyvim.nix
@@ -13,6 +13,22 @@
         kanagawa.enable = pkgs.lib.mkForce false;
       };
 
+      extraConfigLua = ''
+        require('render-markdown').setup({})
+      '';
+
+      extraPlugins = [
+        (pkgs.vimUtils.buildVimPlugin {
+          name = "render-markdown";
+          src = pkgs.fetchFromGitHub {
+            owner = "MeanderingProgrammer";
+            repo = "markdown.nvim";
+            rev = "78ef39530266b3a0736c48b46c3f5d1ab022c7db";
+            hash = "sha256-mddnBvIrekHh60Ix6qIYAnv10Mu40LamGI47EXk9wSo=";
+          };
+        })
+      ];
+
       keymaps = [
         {
           mode = "x";
@@ -30,9 +46,13 @@
       opts.relativenumber = lib.mkForce false;
 
       plugins = {
-        lsp.servers.clangd = {
-          enable = true;
-          cmd = ["clangd" "--fallback-style=webkit" "--offset-encoding=utf-16"];
+        lsp.servers = {
+          clangd = {
+            enable = true;
+            cmd = ["clangd" "--fallback-style=webkit" "--offset-encoding=utf-16"];
+          };
+          pylsp.enable = false;
+          pyright.enable = pkgs.lib.mkForce false;
         };
         lualine.theme = pkgs.lib.mkForce "base16";
         markdown-preview.enable = true;
