9061489e1b9aa9b73ee5f49f652e8c9fa9b93870
just-bqn <self.bqnguyen@gmail.com>
2024-08-29 21:50:10 +0700

    Some commit.

--- templates/python/shell.nix
+++ templates/python/shell.nix
@@ -1,7 +1,17 @@
 {pkgs, ...}: {
+  enterShell = ''
+    export CUDA_PATH=${pkgs.cudatoolkit}
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/run/opengl-driver/lib:/run/opengl-driver-32/lib
+    exec fish
+  '';
+
   env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
     pkgs.stdenv.cc.cc.lib
     pkgs.zlib
+  ];
+
+  packages = [
+    pkgs.cudatoolkit
   ];
 
   languages.python = {
@@ -9,8 +19,10 @@
     venv = {
       enable = true;
       requirements = ''
+        ipykernel
         numpy
         pandas
+        torch
       '';
     };
   };
