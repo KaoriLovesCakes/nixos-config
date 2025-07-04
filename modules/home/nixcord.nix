cbed76befb28886a1d5161fedd7ee2597633bffe
just-bqn <self.bqnguyen@gmail.com>
2025-07-04 12:04:03 +0700

    Some commit.

--- modules/home/nixcord.nix
+++ modules/home/nixcord.nix
@@ -1,9 +1,18 @@
-{
-  programs.nixcord = {
-    enable = true;
-    config = {
-      plugins.fakeNitro.enable = true;
-      themeLinks = ["https://raw.githubusercontent.com/dabluee/discord-themes/refs/heads/main/prsk/nightcordmini.theme.css"];
+{pkgs, ...}: {
+  home = {
+    file = {
+      ".config/BetterDiscord/plugins/LaTeX.plugin.js".text = builtins.readFile (pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/BinaryQuantumSoul/discord-latex/refs/heads/main/dist/LaTeX.plugin.js";
+        hash = "sha256-KMznYwfxutSYgJgmS6mVhbYFOLypXh+Hmj/ziudM71U=";
+      });
+      ".config/BetterDiscord/themes/nightcordmini.theme.css".text = builtins.readFile (pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/dabluee/discord-themes/refs/heads/main/prsk/nightcordmini.theme.css";
+        hash = "sha256-9QuC9YEePSTC0SEzZUf+lYLa9js+E0w7m4PtTJ4OWJI=";
+      });
     };
+    packages = [
+      pkgs.betterdiscordctl
+      pkgs.discord
+    ];
   };
 }
--- modules/nixos/disk/persistence.nix
+++ modules/nixos/disk/persistence.nix
@@ -42,8 +42,6 @@
         "Videos"
         "Virtualisation"
       ];
-      files = [
-      ];
     };
   };
 }
